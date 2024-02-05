035e5ff3d2fe79c946ab2c05f65d818dc1edf80d
EmHeGe <herrerageneremilio@gmail.com>
2024-02-05 20:10:00 +0100

    1 deberia fallar

--- tests/CalculadoraTest.php
+++ tests/CalculadoraTest.php
@@ -6,22 +6,19 @@ final class CalculadoraTest extends TestCase
 {
     public function testSuma()
     {
-        $this->assertEquals(1,1);
-        /*$calc = new Calculadora(3,5);
-        $this->assertEquals(8, $calc->suma());*/
+        $calc = new Calculadora(3,5);
+        $this->assertEquals(8, $calc->suma());
     }
 
     public function testDiffPos()
     {
-        $this->assertEquals(1,1);
-        $calc = new Calculadora(5,3);
+        $calc = new Calculadora(3,5);
         $this->assertEquals(2, $calc->diff());
         
     }
 
     public function testDiffNeg()
     {
-        $this->assertEquals(1,1);
         $calc = new Calculadora(3,5);
         $this->assertEquals(2, $calc->diff());
         
